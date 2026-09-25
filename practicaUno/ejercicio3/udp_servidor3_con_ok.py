63c93bb1c7086bba07b4e8155cbcac78e38fac78
Michal <uo324707@uniovi.es>
2026-09-25 11:11:59 +0000

    ejercicio4

--- practicaUno/ejercicio3/udp_servidor3_con_ok.py
+++ practicaUno/ejercicio3/udp_servidor3_con_ok.py
@@ -2,19 +2,19 @@ import socket
 import sys
 import random
 
-port = int(sys.argv[1]) if len(sys.argv) > 1 else 12345
+port = int(sys.argv[1]) if len(sys.argv) > 1 else 9999
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", port))
 
-print(f"Serwer nasłuchuje na porcie {port}")
+print(f"Server listening on port {port}")
 
 while True:
     datagrama, origen = s.recvfrom(1024)
     if random.randint(0, 1) == 0:
         print("Simulating package lost")
     else:
-        print("Se ha recibido un datagrama desde", origen)
-        print("Contiene lo siguiente:")
+        print("A datagram has been received from", origen)
+        print("It contains the following:")
         print(datagrama.decode("utf-8"))
         s.sendto(b"OK", origen)
